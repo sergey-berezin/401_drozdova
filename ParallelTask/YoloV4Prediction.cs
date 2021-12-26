25bb24cc77e6fa6dc10a1c5611f5393644877e54
DaryaDrozdova <74510194+DaryaDrozdova@users.noreply.github.com>
2021-12-26 20:54:54 +0300

    Adding Task2

--- ParallelTask/YoloV4Prediction.cs
+++ ParallelTask/YoloV4Prediction.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
-namespace ParallelTask.RecognitionLibrary
+namespace ParallelTask
 {
     public class YoloV4Prediction
     {
